08783c7a98dbaa3930be1b1fcc625f429f2299b5
Kateřina Trčková <trckovakaterina@gmail.com>
2020-09-05 21:06:50 +0200

    Implementace interfacu IComparator

---
+++ Lesson_06_DU.Console/Comparator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson_06_DU.App
+{
+    public class Comparator<T> : IComparator<T> where T : IComparable
+    {
+        public HashSet<T> GetGreaterThan(T compare, IEnumerable<T> values)
+        {
+            HashSet<T> greaterThan = new HashSet<T>();
+
+            foreach (var value in values)
+            {
+                if (value.CompareTo(compare) == 1)
+                {
+                    greaterThan.Add(value);
+                }
+            }
+
+            return greaterThan;
+        }
+
+        public HashSet<T> GetGreaterThanOrEqualTo(T compare, IEnumerable<T> values)
+        {
+            HashSet<T> greaterThanOrEqualTo = new HashSet<T>();
+
+            foreach (var value in values)
+            {
+                if (value.CompareTo(compare) >= 0)
+                {
+                    greaterThanOrEqualTo.Add(value);
+                }
+            }
+
+            return greaterThanOrEqualTo;
+        }
+    }
+}
